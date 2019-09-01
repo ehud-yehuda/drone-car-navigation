9b64373212c0ed2bd48f6952722232e7a7599317
ehud-yehuda <udicohen2@gmail.com>
2019-09-01 22:05:43 +0300

    adding last version to code

--- maze.py
+++ maze.py
@@ -1,68 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from multiprocessing import Queue
 import operator
-import itertools
-import sys
-import os
-import math
 
-DEBUG = True
+
+DEBUG = False
 Plot = True
 
+def load_Data(path, Plot):
 
-
-def getNeighboors(position):
-    i,j = position
-    if i > 0 and j > 0:
-        return [(i-1, j-1), (i, j-1), (i+1, j-1), (i+1, j), (i-1, j), (i+1, j+1), (i, j+1), (i-1, j+1)]
-    elif i > 0:
-        return [(i + 1, j), (i - 1, j), (i + 1, j + 1), (i, j + 1), (i - 1, j + 1)]
-    elif j > 0:
-        return [(i, j - 1), (i + 1, j - 1), (i + 1, j), (i + 1, j + 1), (i, j + 1)]
-    else:
-        return [(i + 1, j), (i + 1, j + 1), (i, j + 1)]
-
-
-def canMove(maze, i, j):
-    if(i >= 0 and j >= 0 and i < maze.shape[0] and j < maze.shape[1]):
-        Val = maze[i,j]
-        if all(Val == [0, 0, 0]) or all(Val == [255, 0, 0]):
-            return True
-    return False
-
-
-def BFS(maze, start, end, visited):
-    #queue = Queue()
-    #queue.put([start])
-
-    frontier = []
-    frontier.append([start])
-    maze[start] = [127, 127, 127]
-
-    while frontier:
-
-        path = frontier.pop(0)
-        position = path[-1]
-
-        if position == end:
-            return path
-
-        for d in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
-            nextposition = list(map(operator.add, position, d))
-            if canMove(maze, nextposition[0], nextposition[1]):
-                new_path = list(path)
-                new_path.append(nextposition)
-                maze[nextposition] = [127, 127, 127]
-                frontier.append(new_path)
-
-
-    return visited
-
-
-def main():
-    path = 'C:\\Users\\owner\\Desktop\\maze\\maze.txt'
-    #load data
     with open(path) as f:
         width, heighet, numObstacle  = [int(x) for x in next(f).split()]
         SourceI, SourceJ, destinationI, destinationJ = [int(x) for x in next(f).split()]
@@ -73,48 +18,93 @@ def main():
     for ObstacleIdx in range(numObstacle):
         maze[obstacle[ObstacleIdx][0]:obstacle[ObstacleIdx][2] + 1, obstacle[ObstacleIdx][1]:obstacle[ObstacleIdx][3] + 1] = [0, 255, 0] #green
 
-    maze[SourceI:, SourceJ,:] = [255, 0, 0] #red
+    maze[SourceI, SourceJ, :] = [255, 0, 0] #red
     maze[destinationI, destinationJ, :] = [255, 0, 0] #red
 
-    maze2solve = maze
+
+
     if Plot:
         plt.figure(1)
         plt.imshow(maze)
         plt.text(SourceJ, SourceI, 'Source')
         plt.text(destinationJ, destinationI, 'Destination')
         plt.title('Maze to solve- Good Luck!')
-        plt.show()
+        plt.show(block=False)
 
-    path = BFS(maze2solve, [SourceI, SourceJ], [destinationI, destinationJ], [])
+    return maze, [SourceI, SourceJ], [destinationI, destinationJ]
 
-    if Plot:
-        plt.figure(2)
-        plt.imshow(maze2solve)
-        plt.text(SourceJ, SourceI, 'Source')
-        plt.text(destinationJ, destinationI, 'Destination')
-        plt.title('used maze!')
-        plt.show()
-
-    a=1
+def canMove(maze, i, j):
+    if(i >= 0 and j >= 0 and i < maze.shape[0] and j < maze.shape[1]):
+        Val = maze[i,j]
+        if all(Val == [0, 0, 0]) or all(Val == [255, 0, 0]):
+            return True
+    return False
 
 
+def BFS(maze, start, end):
 
-if __name__ == '__main__':
-      #main(sys.argv[1], sys.argv[2])
-    main()
+    queue = []
 
-""" while not queue.empty():
+    queue.append([start])
+    maze[start[0], start[1], :] = [127, 127, 127]
 
-        path = queue.get()
+    while queue:
+
+        path = queue.pop(0)
         position = path[-1]
 
         if position == end:
             return path
 
-        for neighboor in getNeighboors(position):
-            i,j = neighboor
-            if canMove(maze, i, j):
-                maze[i,j] = [127, 127, 127]
+        for neighboor in [(0, -1), (-1, 0), (1, 0), (0, 1), (1, -1), (1, 1), (-1, 1), (-1, -1)]:
+            nextposition = list(map(operator.add, position, neighboor))
+            if canMove(maze, nextposition[0], nextposition[1]):
                 new_path = list(path)
-                new_path.append(neighboor)
-                queue.put(new_path)"""
+                new_path.append(nextposition)
+                maze[nextposition[0], nextposition[1], :] = [127, 127, 127] # mark as visited
+                queue.append(new_path)
+
+
+    print("I'm exahusted and didn't found my way...")
+    return start.append(end)
+
+def main():
+    #path = 'C:\\Users\\owner\\Desktop\\maze\\maze.txt'
+    path = 'maze.txt'
+    #outputPath = 'C:\\Users\\owner\\Desktop\\maze\\ShortestPath.txt'
+    outputPath = 'ShortestPath.txt'
+    #load data
+
+    maze, start, destination = load_Data(path, Plot)
+
+    shortestpath = BFS(maze, start, destination)
+
+    if(shortestpath):
+        maze, start, destination = load_Data(path, Plot) #The maze got dirty because all the visiting, therfore loading a new one
+
+        for point in shortestpath:
+            maze[point[0], point[1], :] = [0, 0, 255] #blue is the shortest path
+
+        if Plot:
+            plt.figure(2)
+            plt.imshow(maze)
+            plt.text(start[1], start[0], 'Source')
+            plt.text(destination[1], destination[0], 'Destination')
+            plt.title('maze with the shortes path!')
+            plt.show (block=False)
+
+        distance = len(shortestpath)
+        shortestpath.append(distance)
+
+        with open(outputPath, 'w') as f:
+            for point in shortestpath:
+                f.write("%s\n" % point)
+        f.close()
+    else:
+        with open(outputPath, 'w') as f:
+                f.write("%s\n" % "Didn't found any solution")
+        f.close()
+
+if __name__ == '__main__':
+    main()
+
